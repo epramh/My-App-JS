1f15d734ba9d787a6145150485dafa3a12044a0b
epramh <101051721+epramh@users.noreply.github.com>
2023-08-28 16:53:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My-App-JS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-xlzhqz)
---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Hello!');
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
