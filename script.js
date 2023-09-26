f1beeb532e9c7845aa3919286cabba9809c503d5
epramh <101051721+epramh@users.noreply.github.com>
2023-09-26 13:16:29 +0300

    Add responsive bar

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
